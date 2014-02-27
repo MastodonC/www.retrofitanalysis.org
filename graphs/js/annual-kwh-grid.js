7373e3dd3dd2aa7d536f36b95f911e9167d1e4ab
Bruce Durling <bld@rosetta.(none)>
2014-02-27 17:04:34 +0000

    A bit of a hack to make SSL work w/Amazon's certs.

---
+++ graphs/js/annual-kwh-grid.js
@@ -0,0 +1,185 @@
+var annual_grid_energy = function annual_grid_energy(div) {
+    var margin = {top: 20, right: 40, bottom: 50, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .rangeRound([height, 0]);
+
+    var y2 = d3.scale.linear()
+        .rangeRound([height, 0]);
+
+    var color = d3.scale.ordinal()
+        .range(['#6998bc', '#de6363']);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(d3.format(".2s"));
+
+    var y2Axis = d3.svg.axis()
+        .scale(y2)
+        .orient("right")
+        .tickFormat(d3.format(".0f"));
+
+    var tooltip = d3.select("body")
+	.append("div")
+        .attr("id", "tooltip")
+	.style("position", "absolute")
+	.style("z-index", "10")
+	.style("visibility", "hidden");
+
+
+    // Define the line
+    var valueline = d3.svg.line()
+    // we want the point to be in the centre of the rangeband for the bar chart.
+        .x(function(d) { return x(d.property)  + x.rangeBand() / 2; })
+        .y(function(d) { return y2(d.annual_emissions); })
+        .interpolate("linear");
+
+    var svg = d3.select(div).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.tsv("/www.retrofitanalysis.org/data/emissions.csv", function(error, data) {
+        color.domain(["electrical_per_area", "gas_per_area"]);
+
+        data.forEach(function(d) {
+            var y0 = 0;
+            d.emissions = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+            d.total = d.emissions[d.emissions.length - 1].y1;
+            d.annual_emissions = +d.annual_emissions;
+        });
+
+        data.sort(function(a, b) { return a.annual_emissions - b.annual_emissions; });
+        x.domain(data.map(function(d) { return d.property; }));
+        y.domain([0, 40 + d3.max(data, function(d) { return d.total; })]).nice();
+        y2.domain([0, 10 + d3.max(data, function(d) { return d.annual_emissions; })]).nice();
+
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .attr("y", 9)
+            .attr("x", 0)
+            .attr("dy", ".71em")
+            .attr("transform", "rotate(-45)")
+            .style("text-anchor", "end");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("kWh / m2 / year");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + width + ",0)")
+            .call(y2Axis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -12)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("kg carbon dioxide / m2 / year");
+
+        var numberFormat = d3.format(".2f");
+        
+        var property = svg.selectAll(".property")
+            .data(data)
+            .enter().append("g")
+            .attr("class", "emissions")
+            .attr("transform", function(d) { return "translate(" + x(d.property) + ",0)"; })
+            .on("mouseover", function() { return tooltip.style("visibility", "visible");})
+            .on("mousemove", function() {
+                return tooltip.style("top", (d3.event.pageY-10)+"px").style("left", (d3.event.pageX+10)+"px").html('Electrical per Area: ' + numberFormat(d3.event.currentTarget.__data__.electrical_per_area) + 'kWh/m<sup>2</sup><br />Gas per Area: ' + numberFormat(d3.event.currentTarget.__data__.gas_per_area) + 'kWh/m<sup>2</sup><br />Total Energy: ' + numberFormat(d3.event.currentTarget.__data__.energy_per_area) + 'kWh/m<sup>2</sup><br />' )
+                // .style("height", "38px");
+            })
+            .on("mouseout", function() { return tooltip.style("visibility", "hidden");});
+
+        property.selectAll("rect")
+            .data(function(d) { return d.emissions; })
+            .enter().append("rect")
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) { return y(d.y1); })
+            .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+            .style("fill", function(d) { return color(d.name); });
+
+        // Add the valueline path.
+        svg.append("path")	
+            .attr("class", "line")
+            .attr("d", valueline(data));
+        
+        svg.selectAll("dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("r", 3.5)
+            .attr("cx", function(d) { return x(d.property)  + x.rangeBand() / 2; })
+            .attr("cy", function(d) { return y2(d.annual_emissions); })
+            .on("mouseover", function() { return tooltip.style("visibility", "visible");})
+            .on("mousemove", function() {
+                return tooltip.style("top", (d3.event.pageY-10)+"px").style("left", (d3.event.pageX+10)+"px").html('Annual Emissions: ' + numberFormat(d3.event.currentTarget.__data__.annual_emissions) + 'kgCO<sub>2</sub>/m<sup>2</sup>')
+            })
+            .on("mouseout", function() { return tooltip.style("visibility", "hidden");});
+
+        var legend = svg.selectAll(".legend")
+            .data(color.domain().reverse())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(-825," + i * 20 + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 36)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color);
+
+        legend.append("text")
+            .attr("x", width - 10)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "start")
+            .text(function(d) {
+                console.log(d);
+                return d == 'electrical_per_area'  ? 'Grid electricity' :
+                    d == 'gas_per_area'  ?  'Grid Gas' :
+                    'Unexpected Value';
+            });
+
+    });
+
+    var legend2 = svg.selectAll(".legend2")
+        .data(["annual_emisions"])
+        .enter().append("g")
+        .attr("class", "legend2")
+        .attr("transform", function(d, i) { return "translate(-25," + i * 20 + ")"; });
+
+    legend2.append("rect")
+        .attr("x", width - 10)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", "#6ebd61");
+
+    legend2.append("text")
+        .attr("x", width - 15)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text("Annual carbon dioxide emissions per m2");
+
+}
---
+++ graphs/js/pp-air-tightness.js
@@ -0,0 +1,113 @@
+var pp_air_tightness = function pp_air_tightness(div) {
+    var margin = {top: 20, right: 80, bottom: 50, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var color = d3.scale.category10();
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .ticks(0)
+        .orient("left");
+
+    var line = d3.svg.line()
+        .interpolate("basis")
+        .x(function(d) { return x(d.number); })
+        .y(function(d) { return y(d.tightness); });
+
+    var svg = d3.select(div).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.tsv("/www.retrofitanalysis.org/data/airtightness.csv", function(error, data) {
+        color.domain(d3.keys(data[0]).filter(function(key) { return key !== "number"; }));
+
+        data.forEach(function(d) {
+            d.number = +d.number;
+        });
+
+        var metric = color.domain().map(function(name) {
+            return {
+                name: name,
+                values: data.map(function(d) {
+                    return {number: d.number, tightness: +d[name]};
+                })
+            };
+        });
+
+        x.domain(d3.extent(data, function(d) { return d.number; }));
+
+        y.domain([
+            d3.min(metric, function(c) { return d3.min(c.values, function(v) { return v.tightness; }); }),
+            d3.max(metric, function(c) { return d3.max(c.values, function(v) { return v.tightness; }); })
+        ]).nice();
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .append("text")
+            .attr("x", width)
+            .attr("y", 40)
+            .style("text-anchor", "end")
+            .text("Air-tightness - m3 / m2 / hr at 50Pa");
+
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Relative frequency");
+
+        var series = svg.selectAll(".series")
+            .data(metric)
+            .enter().append("g")
+            .attr("class", "series");
+
+        series.append("path")
+            .attr("class", "line")
+            .attr("d", function(d) { return line(d.values); })
+            .style("stroke", function(d) { return color(d.name); });
+
+        var legend = svg.selectAll(".legend")
+            .data(color.domain().slice().reverse())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) {
+                return "translate(0," + i * 20 + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 36)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color);
+
+        legend.append("text")
+            .attr("x", width - 10)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "start")
+            .text(function(d) {
+                return d == 'post_air_tightness'  ? 'Post-retrofit' :
+                    d == 'pre_air_tightness'  ?  'Pre-retrofit' :
+                    'Unexpected Value';
+            });
+
+    });
+}
